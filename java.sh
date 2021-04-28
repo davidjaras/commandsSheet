28707fe0491a1cf1639c4a3be607ac43fc2494af
Santiago Sanchez Taborda <santiago.sanchez.t@gmail.com>
2021-04-28 17:35:23 -0500

    feat(os): Add openssl commands

--- java.sh
+++
@@ -1,8 +0,0 @@
-## List Java procceses running
-jps
-
-## Threadup of the JVM
-jstack -l <PID>
-
-## Print the memory statistics of the JVM
-jmap -dump:file=DumpFile.txt <process-id>
--- os.sh
+++ os.sh
@@ -66,12 +66,58 @@ gpg -e -r journalists filename.txt
 # Decrypt a file with GPG
 gpg --decrypt filename.txt.gpg
 
+# Generate random base64
+openssl rand -base64 ${SIZE:-16}
+
+# Generate a new private key and Certificate Signing Request
+openssl req -out CSR.csr -new -newkey rsa:2048 -nodes -keyout privateKey.key
+
+# Generate a self-signed certificate (CRT)
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt
+
+# Generate a certificate signing request (CSR) for an existing private key
+openssl req -out CSR.csr -key privateKey.key -new
+
+# Generate a certificate signing request based on an existing certificate
+openssl x509 -x509toreq -in certificate.crt -out CSR.csr -signkey privateKey.key
+
+# Generate RSA keys
+openssl genrsa -out private.pem 1024
+
+# Remove a passphrase from a private key
+openssl rsa -in privateKey.pem -out newPrivateKey.pem
+
+# Check PEM file passphrase
+openssl pkey -in brodcast.pem
+
+# Check a Certificate Signing Request (CSR)
+openssl req -text -noout -verify -in CSR.csr
+
+# Check a private key
+openssl rsa -in privateKey.key -check
+
+# Check a certificate X509
+openssl x509 -in certificate.crt -text -noout
+
+# Check a certificate from P12 file
+openssl pkcs12 -info -nodes -in yourfilename.p12 
+
+# Convert P12 to PEM
+## If you want to use a password to encrypt the key file, you must remove the -nodes flag
+## You can add -nocerts to only output the private key or add -nokeys to only output the certificates.
+openssl pkcs12 -in path.p12 -out newfile.pem -nodes
+
+# Convert DER to PEM
+openssl x509 -inform der -in certificate.cer -out certificate.pem
+
+# Convert PEM to DER
+openssl x509 -outform der -in certificate.pem -out certificate.der
+
+# Convert PEM to CRT (.CRT file)
+openssl x509 -outform der -in certificate.pem -out certificate.crt
+
 # Get SSL certifificate
 openssl s_client -showcerts -connect runtime-manager.anypoint.mulesoft.com:443 2>/dev/null | openssl x509 -noout -text
-
-# Get certificate content of a file
-openssl x509 -in ${cert_file} -text -noout
-
 
 # Get the number of file descriptors opened
 lsof | wc -l
