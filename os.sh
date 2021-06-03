cf29bbe8dbbf7345f03e41c62c5dd4c4933deff9
Santiago Sanchez Taborda <santiago.sanchez.t@gmail.com>
2021-06-03 16:25:27 -0500

    feat(os): Add new openssl command

--- os.sh
+++ os.sh
@@ -121,8 +121,12 @@ openssl x509 -outform der -in certificate.pem -out certificate.der
 # Convert PEM to CRT (.CRT file)
 openssl x509 -outform der -in certificate.pem -out certificate.crt
 
+# Get public key from x509 cer
+openssl x509 -in certificate.pem -pubkey -noout > public_key.pem
+
 # Get SSL certifificate
 openssl s_client -showcerts -connect runtime-manager.anypoint.mulesoft.com:443 2>/dev/null | openssl x509 -noout -text
+
 
 # Get the number of file descriptors opened
 lsof | wc -l
